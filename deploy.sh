38b30b04a073cf3be1ce95b5108d8156d69755d6
noperator <noper@tor.fyi>
2025-04-16 17:32:12 -0400

    support ext deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ export PATH="$(find "$HOME/.nvm/versions/node" -maxdepth 1 -type d | sort -V | t
 cd "$(dirname "$0")"
 
 TEXT_FILE='free.txt'
+EXT_TEXT_FILE='ext.txt'
 DEPLOY_DIR='deploy'
 CAL_DIR='cal'
 DATE=$(date -Iseconds -u | sed -E 's/\+00:00/Z/')
@@ -14,9 +15,13 @@ echo "$DATE"
 
 echo -e "last updated $DATE\n" >"$TEXT_FILE"
 
+echo -e "last updated $DATE\n" >"$EXT_TEXT_FILE"
+
 mkdir -p "$DEPLOY_DIR"
+mkdir -p "$DEPLOY_DIR/ext"
 mkdir -p "$CAL_DIR"
 rm "$DEPLOY_DIR"/* 2>/dev/null
+rm "$DEPLOY_DIR/ext"/* 2>/dev/null
 rm "$CAL_DIR"/* 2>/dev/null
 
 source .env
@@ -26,10 +31,19 @@ for CAL_URL in "${CAL_URLS[@]}"; do
 done
 
 source venv/bin/activate
+
 python3 main.py \
+    -s $(date -Idate -d 'yesterday') \
     -f $(find "$CAL_DIR" -type f -name '*.ics*') |
     grep -E '^$|^[A-Za-z]{3} {1,2}[0-9]{1,2} [A-Za-z]{3} @ {1,2}[0-9:]{4,5} [AP]M – {1,2}[0-9:]{4,5} [AP]M [A-Za-z0-9+-/_]{2,32} \(([0-9]{1,2}h)?([0-9]{1,2}m)?\)' \
         >>"$TEXT_FILE"
+
+python3 main.py \
+    -s $(date -Idate -d 'yesterday') \
+    -w \
+    -f $(find "$CAL_DIR" -type f -name '*.ics*') |
+    grep -E '^$|^[A-Za-z]{3} {1,2}[0-9]{1,2} [A-Za-z]{3} @ {1,2}[0-9:]{4,5} [AP]M – {1,2}[0-9:]{4,5} [AP]M [A-Za-z0-9+-/_]{2,32} \(([0-9]{1,2}h)?([0-9]{1,2}m)?\)( {1,4}(morn|even|wknd( (morn|even))?))?' \
+        >>"$EXT_TEXT_FILE"
 
 cat >"$DEPLOY_DIR/index.html" <<EOF
 <!DOCTYPE html>
@@ -51,6 +65,31 @@ cat >"$DEPLOY_DIR/index.html" <<EOF
 <body>
     <pre>
 $(cat "$TEXT_FILE")
+    </pre>
+</body>
+</html>
+EOF
+
+cat >"$DEPLOY_DIR/ext/index.html" <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>free (extended)</title>
+    <style>
+        pre {
+            font-family: monospace;
+            white-space: pre-wrap;
+        }
+        body {
+            background: #f5f5f5;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <pre>
+$(cat "$EXT_TEXT_FILE")
     </pre>
 </body>
 </html>
